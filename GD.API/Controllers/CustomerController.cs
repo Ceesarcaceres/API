bd342def57a1fc9f671d07d59a5e5f34813fb956
Cesar <cesar@gestaodinamica.com.br>
2021-02-10 18:30:36 -0300

    Add project files.

---
+++ GD.API/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET api
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "customer";
+        }
+
+        //POST api/
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ GD.UI/wwwroot/js/Customer.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+  
+    $.ajax({
+        method: "POST",
+        url: "Customer.js",
+        //data: { nome: "Pedro", email: "pedro@email.com" }
+    })
+
+//    $(document).ready(function () {
+//    let endpoint = 'api/[controller]'
+//    $(".content a").each(function (index, element) {
+
+//        $.ajax({
+//            url: endpoint + "&q=" + $(this).text(),
+//            contentType: "application/json",
+//            dataType: 'json',
+//            success: function (result) {
+//                console.log(result);
+//            }
+//        })
+//    });
+//});
